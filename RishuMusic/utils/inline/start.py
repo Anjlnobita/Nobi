5f1c42fd5e50516cc7b9a090937976ed097b6917
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-10 06:58:01 +0530

    Update start.py

--- RishuMusic/utils/inline/start.py
+++ RishuMusic/utils/inline/start.py
@@ -10,31 +10,28 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
     ]
     return buttons
 
 
+
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_3"],url=f"https://t.me/{app.username}?startgroup=true",)
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
         ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+      ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_CP"),
-        ],
-        
-        [
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="ALLBOT_CP"),
-            InlineKeyboardButton(text=_["S_B_11"], callback_data="PROMOTION_CP"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
     ]
     return buttons
