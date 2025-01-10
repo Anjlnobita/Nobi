2115eec148512b25a3b94cd3c8f1b5b5c89a83f3
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-10 06:19:13 +0530

    Update broadcast.py

--- RishuMusic/plugins/mics/broadcast.py
+++ RishuMusic/plugins/mics/broadcast.py
@@ -20,7 +20,7 @@ from config import adminlist
 IS_BROADCASTING = False
 
 
-@app.on_message(filters.command("broadcast") & filters.user(SUDOERS))
+@app.on_message(filters.command(["maintenance"]) & SUDOERS)
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
